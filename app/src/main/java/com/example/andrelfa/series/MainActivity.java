3a980edf0147c622febbe73575fc2e36f043763a
Andre LFA <andrelfaareas@gmail.com>
2017-10-25 14:49:10 -0200

    Primeira parte do aplicativo

---
+++ app/src/main/java/com/example/andrelfa/series/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.andrelfa.series;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String[] series = {"Game of Thrones", "Mr. Robot", "This is Us", "Rick and Morty"};
+
+        ListView listaSeries = (ListView) findViewById(R.id.lista_series);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, series);
+        listaSeries.setAdapter(adapter);
+    }
+}
