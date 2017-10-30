3c5d474465ddee063a6f32853e09a3562a114adf
Andre LFA <andrelfaareas@gmail.com>
2017-10-30 12:04:39 -0200

    Alterando o botao de activity para que leve para o formulario de review

--- app/src/main/java/com/example/andrelfa/series/SerieActivity.java
+++ app/src/main/java/com/example/andrelfa/series/SerieActivity.java
@@ -1,7 +1,10 @@
 package com.example.andrelfa.series;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 
 /**
@@ -13,5 +16,14 @@ public class SerieActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_of_thrones);
+
+        Button botaoReview = (Button)(findViewById(R.id.novo_review));
+        botaoReview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent myIntent = new Intent(view.getContext(), FormularioActivity.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
     }
 }
