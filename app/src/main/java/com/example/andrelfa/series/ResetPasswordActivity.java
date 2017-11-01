f1d0a59f3910d107487b11835f6fba35df6d8de3
Andre LFA <andrelfaareas@gmail.com>
2017-11-01 11:02:44 -0200

    Testando login com firebase

---
+++ app/src/main/java/com/example/andrelfa/series/ResetPasswordActivity.java
@@ -0,0 +1,8 @@
+package com.example.andrelfa.series;
+
+/**
+ * Created by JuniorA02 on 11/1/2017.
+ */
+
+public class ResetPasswordActivity {
+}
---
+++ app/src/main/java/com/example/andrelfa/series/SignupActivity.java
@@ -0,0 +1,10 @@
+package com.example.andrelfa.series;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by JuniorA02 on 11/1/2017.
+ */
+
+public class SignupActivity extends AppCompatActivity {
+}
