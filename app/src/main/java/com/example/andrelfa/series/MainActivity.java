4bce62264faebb08eb7fb427ad7f8a0dff5f8603
Andre LFA <andrealfaareas@gmail.com>
2017-11-11 13:32:49 -0200

    Adiciona a segunda versão do ToolBar

--- app/src/main/java/com/example/andrelfa/series/MainActivity.java
+++ app/src/main/java/com/example/andrelfa/series/MainActivity.java
@@ -2,15 +2,32 @@ package com.example.andrelfa.series;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
+
+    protected void Toolbar () {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        toolbar.setTitle("NERVERMORE");
+        toolbar.setSubtitle("Serialize-se!");
+        toolbar.setLogo(R.mipmap.ic_launcher);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar();
+
 
         //Intent myIntent = new Intent(view.getContext(), SerieActivity.class);
         //startActivityForResult(myIntent, 0);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //getMenuInflater().inflate();
+        return super.onCreateOptionsMenu(menu);
     }
 }
