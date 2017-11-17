36bedf6fafb127944fcdfea6ecfd8f358cb1a7cf
Andre LFA <andrelfaareas@gmail.com>
2017-11-17 11:27:57 -0200

    Adicionando o Spinner no review

--- app/src/main/java/com/example/andrelfa/series/FormularioActivity.java
+++ app/src/main/java/com/example/andrelfa/series/FormularioActivity.java
@@ -5,7 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,6 +17,12 @@ public class FormularioActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_formulario);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner_serie);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.nota_array, R.layout.support_simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource(R.layout.support_simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
 
         Button botaoSalvar = (Button) findViewById(R.id.botao_salvar);
         botaoSalvar.setOnClickListener(new View.OnClickListener() {
